684b7e5297a253c33f499500c9d9446d656aa4f9
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-03-30 18:23:13 +0900

    [Update]new code

--- Day1/AcademyJava/Src/Day3/One.java
+++ Day1/AcademyJava/Src/Day3/One.java
@@ -3,7 +3,13 @@ package Day3;
 public class One {
 
 	public static void main(String[] args) {
-
+		int total = 0, x = 0, y;
+		while(++x < 5) {
+			y = x*x;
+			System.out.println(y);
+			total+=y;
+		}
+		System.out.println("총합은 " + total);
 	}
-
 }
+
