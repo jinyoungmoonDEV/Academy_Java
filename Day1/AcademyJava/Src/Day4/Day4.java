c227084e52bf1c9cfea8bfd84387af118ee1f787
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-04-02 13:12:35 +0900

    [Update]new code

--- Day1/AcademyJava/Src/Day4/Day4.java
+++ Day1/AcademyJava/Src/Day4/Day4.java
@@ -1,9 +1,10 @@
 package Day4;
+import java.io.PrintStream;
 import java.util.Scanner;
 public class Day4 {
 
 	public static void main(String[] args) {
-		
+
 		/*
 		//1번
 		Scanner sc = new Scanner(System.in);
@@ -12,7 +13,8 @@ public class Day4 {
 			System.out.print("성년");
 		else
 			System.out.print("미성년");
-	    */
+		 */
+
 		
 		/*
 		//2번
@@ -26,7 +28,8 @@ public class Day4 {
 			System.out.print("보통입니다");
 		else
 			System.out.print("노력해야겠습니다.");
-		*/
+		 */
+
 		
 		/*
 		//3번
@@ -42,7 +45,8 @@ public class Day4 {
 			continue;
 		}while(num > 0);
 		System.out.printf("입력한 양의 정수 중에서 짝수의 합은 %d", hap);
-		*/
+		 */
+
 		
 		/*
 		//4번
@@ -54,8 +58,9 @@ public class Day4 {
 			}
 			System.out.print("\n");
 		}
-		*/
-		 
+		 */
+
+		
 		//5번
 		/*
 		int a,b,c;
@@ -68,8 +73,122 @@ public class Day4 {
 		else if(a*a + b*b == c*c){
 			System.out.printf("a=%d, b=%d, c=%d", a,b,c);
 		}
+		 */
+
+		
+		//6번+7번
+		/*
+		String c = input("철수");
+		String y = input("영희");
+		whoWin(c,y);
 		*/
 		
-		//6번
+		
+		/*
+		//8번(코드 main메소드 아래)
+		System.out.println(factorial(5));
+		 */
+
+		/*
+		//9번
+		foo("안녕",1);
+		foo("안녕하세요",1,2);
+		foo("잘 있어");
 	}
+	static void foo(String a) {
+		System.out.println(a);
+	}
+	static void foo(String s,int a) {
+		System.out.printf("%s %d\n",s,a);
+	}
+	static void foo(String s,int a,int b) {
+		System.out.printf("%s %d %d\n",s,a,b);
+	}
+		 */
+		
+		
+		//10번
+		/*
+		System.out.print("양의 정수릏 입력하세요 : ");
+		int num = new Scanner(System.in).nextInt();
+		if(isPrine(num))
+			System.out.println(num + "는 소수 입니다.");
+		else
+			System.out.println(num + "는 소수가 아닙니다.");
+		 */
+	}
+	
+	
+	/*8번
+	static int factorial(int n) {
+		return switch (n) {
+		case 1 -> 1;
+		case 2 -> 2;
+		case 3 -> 3;
+		case 4 -> 4;
+		case 5 -> 5;
+		default -> 18;
+		};
+	}
+	 */
+	
+	
+	/*//10번
+	public static boolean isPrine(int num) {
+		int count = 0;
+		for(int i=1;i<=num;i++) {
+			if(num%i == 0) {
+				count++;
+			}
+		}
+		if(count == 2) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	 */
+	
+	
+     /*
+      //6,7번
+	public static void whoWin(String c, String y) {
+		Scanner sc = new Scanner(System.in);
+		String s1, s2;
+		System.out.printf("%s : ",c);
+		s1 = sc.next();
+		System.out.printf("%s : ",y);
+		s2 = sc.next();
+
+		if (s1.equals("r")) {
+			if (s2.equals("s"))
+				System.out.println("철수, 승!");
+			else if (s2.equals("p"))
+				System.out.println("영희, 승!");
+			else
+				System.out.println("무승부");
+		} else if (s1.equals("s")) {
+			if (s2.equals("p"))
+				System.out.println("철수, 승!");
+			else if (s2 == "r")
+				System.out.println("영희, 승!");
+			else
+				System.out.println("무승부");
+		} else if (s1.equals("p")) {
+			if (s2.equals("r"))
+				System.out.println("철수, 승!");
+			else if (s2.equals("s"))
+				System.out.println("영희, 승!");
+			else
+				System.out.println("무승부");
+		}
+	}
+	
+
+	public static String input(String string) {
+		return string;
+	}
+	*/
 }
+
--- Day1/AcademyJava/Src/Day4/rsp.java
+++ Day1/AcademyJava/Src/Day4/rsp.java
@@ -8,7 +8,7 @@ public class rsp {
 		String b = sc.next();
 		if(a.equals(b)){
 			System.out.print("무승부");
-		}
+		}  
 		else {
 			if(a.equals("r")) {
 				if(b.equals("s")) {
@@ -16,23 +16,22 @@ public class rsp {
 				}
 				else
 					System.out.print("영희, 승!");
-				}
+			}
 			if(a.equals("s")) {
 				if(b.equals("p")) {
 					System.out.print("철수, 승!");
 				}
 				else
 					System.out.print("영희, 승!");
-				}
+			}
 			if(a.equals("p")) {
 				if(b.equals("r")) {
 					System.out.print("철수, 승!");
 				}
 				else
 					System.out.print("영희, 승!");
-				}
-			else 
 			}
+		}
 	}
 
 }
